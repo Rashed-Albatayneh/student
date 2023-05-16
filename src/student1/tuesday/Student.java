0a46d29a8cc649efd8fb17ba82cbf0fc5abbacb7
Rashed Albatayneh <miskmbob69@gmail.com>
2023-05-16 09:59:29 -0400

    first comment

---
+++ src/student1/tuesday/Student.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package student1.tuesday;
+
+/**
+ *
+ * @author miskm
+ */
+class Student {
+       private String sname;
+    private String sID;
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public String getsID() {
+        return sID;
+    }
+
+    public void setsID(String sID) {
+        this.sID = sID;
+    }
+
+    public Student(String sID, String sname) {
+        this.sname = sname;
+        this.sID = sID;
+    }
+}
---
+++ src/student1/tuesday/Student1Tuesday.java
@@ -0,0 +1,28 @@
+
+package student1.tuesday;
+
+import java.util.Scanner;
+
+
+public class Student1Tuesday {
+
+    
+    public static void main(String[] args) {
+//   Student s1 = new Student(sID:"s1",name:"Ronak";);
+      
+//      Scanner input = new Scanner(System.in);
+      
+      Student[] studentList= new Student[3];
+      studentList[0]=new Student("s2","xyz");
+      studentList[1] =new Student("s2","abc");
+      studentList[2] = new Student("s3","abc");
+      
+      for(int i = 0;i<studentList.length;i++){
+        System.out.println(studentList[i].getSname());
+      }
+      
+      
+      
+    }
+    
+}
